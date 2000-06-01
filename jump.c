a128d770f235be7e50102d136f80178e79034d31
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2000-06-01 14:27:47 +0900

    Initial revision

---
+++ jump.c
@@ -0,0 +1,317 @@
+/*
+ * jump-to-error
+ *
+ * Written by bsh, Dec 1991.
+ */
+
+/* $Id: jump.c,v 1.1 2000/06/01 05:27:47 amura Exp $ */
+
+/* $Log: jump.c,v $
+/* Revision 1.1  2000/06/01 05:27:47  amura
+/* Initial revision
+/*
+*/
+
+#include "config.h"
+#ifdef	JUMPERR
+
+#include "def.h"
+#ifndef BSD
+# ifndef index
+#  define index(s,c)	strchr(s,c)
+# endif
+#endif	/* BSD */
+
+#ifdef	REGEX_JAPANESE
+#include "regex_j.h"
+#else
+#include "regex_e.h"
+#endif
+#define BYTEWIDTH 8
+
+#ifndef	R_OK              /* for access() */
+#  define R_OK 4
+#endif
+
+static struct re_pattern_buffer re_buff;
+static char fastmap[(1 << BYTEWIDTH)];
+static char compile_command[NLINE]	=  "make";
+			/* "compile" command buffer.			*
+			 * This	variable is *NOT* buffer local, this is *
+			 * global. 	Y.Koyanagi			*/
+
+/* Regular expression for filename/linenumber in error in compilation log.
+ * It mathes
+ *     filename, line 1234     or
+ *     filename: 1234          or
+ *     filename  1234          or
+ *     filename(1234)          or
+ *     1234 of filename        or
+ *     filename at line 1234
+ */
+#if defined(AMIGA)
+#define	DEFAULT_REGEXP   "\\(\\([^ \n]+:\\)?[^ \n]+\\(:? *\\| at line \\|, line \\|(\\)[0-9]+\\)\\|\\([0-9]+ *of *[^ \n]+\\)"
+#elif defined(MSDOS)||defined(HUMAN68K)||defined(_WIN32)
+#define	DEFAULT_REGEXP   "\\(\\([a-zA-Z]:\\)?[^ :\n]+\\(:? *\\| at line \\|, line \\|(\\)[0-9]+\\)\\|\\([0-9]+ *of *[^ \n]+\\)"
+#else
+#define	DEFAULT_REGEXP   "\\([^ :\n]+\\(:? *\\| at line \\|, line \\|(\\)[0-9]+\\)\\|\\([0-9]+ *of *[^ \n]+\\)"
+#endif
+/*
+#define	DEFAULT_REGEXP   "\\([^ :\n]+\\(:? *\\|, line \\|(\\)[0-9]+\\)\\|\\([0-9]+ *of *[^ \n]+\\|[^ \n]+ \\(at \\)*line [0-9]+\\)"
+*/
+
+static	char *grab_filename();
+
+/*
+ *
+ */
+set_regexp( pat )
+    char *pat;
+{
+    char *message;
+    
+    re_buff.allocated = 40;
+    re_buff.buffer = (char *) malloc (re_buff.allocated);
+    if( re_buff.buffer == NULL ){
+	ewprintf( "Can't get %d bytes", re_buff.allocated );
+	re_buff.allocated = 0;
+	return FALSE;
+    }
+    re_buff.fastmap = fastmap;
+    re_buff.translate = NULL;
+    message = re_compile_pattern (pat, strlen(pat), &re_buff);
+    if (message != '\0') {
+	ewprintf("Regex Error: %s", message);
+	free( re_buff.buffer );
+	re_buff.allocated = 0;
+	return(FALSE);
+    }
+    re_compile_fastmap (&re_buff);
+    return TRUE;
+}
+
+#define	BUFLEN	100
+parse_error_message( clp, col, namebuf, ip, parse_end )
+    LINE *clp;
+    char *namebuf;
+    int *ip, *parse_end;
+{
+    struct re_registers regs;
+    char buf[BUFLEN+1];
+    int i, len;
+    char *filename;
+    
+    if( re_buff.allocated == 0 && !set_regexp( DEFAULT_REGEXP ) )
+	return FALSE;
+
+    i = re_search (&re_buff, ltext(clp), llength(clp),
+		   col, llength(clp), &regs);
+     if (i < 0)
+	 return FALSE;
+
+    if( parse_end ) *parse_end = regs.end[0]+1;
+    len = regs.end[0] - regs.start[0];
+    if( len > BUFLEN ) len = BUFLEN;
+    strncpy( buf, ltext(clp) + regs.start[0], len );
+    buf[len] = '\0';
+
+    for(i=len; i>0 && ISDIGIT(buf[i-1]); --i )
+	;
+    if( i < len ){
+	/* we are looking filename-first style. */
+	*ip = atoi( &buf[i] );
+	buf[i] = '\0';
+	filename = grab_filename( buf );
+    }
+    else {
+	/* line-number-first style */
+	*ip = atoi(buf);
+	while( ISDIGIT(buf[i]) || buf[i] == ' ' || buf[i] == '\t' )
+	    ++i;
+	filename = grab_filename( buf );
+    }
+    if( *filename ){
+	strcpy( namebuf, filename );
+	return TRUE;
+    }
+    return FALSE;
+}    
+
+/*
+ * jump-to-error
+ * Parse current line as a error message, then vist correspoding source code.
+ */
+jumptoerror(f,n)
+{
+    int  lineno;
+    char buf[BUFLEN+1];
+    char *p=buf;
+    char cmdbuf[200];
+    int col;
+    LINE *dlp;
+    extern int gotoline();
+	
+    dlp = curwp->w_dotp;
+    while (dlp != curbp->b_linep)
+    {
+    /* get filename and line number to visit */
+	col = 0;
+	while (col < llength(dlp) &&
+	       parse_error_message(dlp, col, buf, &lineno, &col ) )
+	{
+	    if (0 == access( buf, R_OK )){
+		/* ewprintf( "file:`%s' line %d", buf, lineno ); */
+		/*
+		 * All the hairly works to give filename to filevisit() is
+		 * done by excline().
+		 */
+		while (*p) {
+		    if (*p == '\\')
+			*p = '/';
+		    p++;
+		}
+		sprintf( cmdbuf, "(%s \"%s\")",
+			(f&FFARG)?"find-file":"find-file-other-window", buf);
+		curwp->w_flag |= WFHARD;
+		curwp->w_doto = 0;
+		curwp->w_linep = dlp;
+		curwp->w_lines = 0;
+		if (lforw(dlp) != curbp->b_linep)
+		    curwp->w_dotp = lforw(dlp);
+		if (!excline(cmdbuf))
+		    return FALSE;
+		gotoline( FFARG, lineno );
+		return TRUE;
+	    }
+	}
+	
+	dlp = lforw(dlp);
+    }
+    curwp->w_flag |= WFHARD;
+    curwp->w_doto = 0;
+    curwp->w_dotp = lback(curbp->b_linep);
+    curwp->w_linep = lback(curbp->b_linep);
+    curwp->w_lines = 0;
+    
+    ewprintf( "No more errors." );
+    return FALSE;
+}
+
+/*
+ * extract filename removing punctuations around.
+ */
+static char *
+grab_filename( buf )
+    char *buf;
+{
+    char *p;
+#ifdef AMIGA
+    int colon_has = FALSE;
+#endif
+
+    if( *buf == '"' ){
+	/* "filename" */
+	for(p=buf+1; *p && *p != '"'; ++p )
+	    ;
+	*p = '\0';
+	return buf+1;
+    }
+
+    p = buf;
+#ifdef AMIGA
+    for(;*p && !index(" \t,(", *p); ++p )
+    {
+	if (*p == ':')
+	{
+	    if (colon_has)
+		break;
+	    else
+		colon_has = TRUE;
+	}
+    }
+    *p = '\0';
+#else
+# if defined(MSDOS)||defined(HUMAN68K)||defined(_WIN32)
+    if (buf[1]==':' && buf[2]!='\0' && (ISUPPER(buf[0])||ISLOWER(buf[0])))
+	p += 2;
+# endif
+    for(;*p && !index(" \t:,(", *p); ++p )
+	;
+    *p = '\0';
+#endif
+    return buf;
+}
+
+/*
+ * COMMAND: compile
+ * 91.09.14  Modified by Y.Koyanagi
+ * "compile" command.
+ */
+/*ARGSUSED*/
+compile(f, n)
+{
+    register BUFFER	*bp, *obp;
+    register WINDOW	*wp, *owp;
+    register int	s;
+    char	 buf[NLINE],*result;
+    extern char *call_process();
+
+    if (compile_command[0] == '\0')
+	s=eread("compile: ", buf, NLINE, EFNEW);
+    else
+	s=eread("compile: (%s) ", buf, NLINE, EFNEW, compile_command);
+    if (s == ABORT) return s;
+    if (s == TRUE) strcpy(compile_command, buf);
+    
+    if ((bp = bfind("*compilation*", TRUE)) == NULL) return FALSE;
+    if ((wp = popbuf(bp)) == NULL) return FALSE;
+    if (bclear(bp) != TRUE) return FALSE;
+    obp = curbp; owp = curwp;
+    curbp = bp; curwp = wp;
+    /* cmode(0,1) */
+    if (addline(bp, compile_command) == FALSE) return FALSE;
+    update();
+    if ((result = call_process(compile_command, NULL)) == NULL)
+	return FALSE;
+    isetmark();
+    s = insertfile(result, (char *)NULL);
+    (VOID) gotobob(0, 1);
+    bp->b_dotp = wp->w_dotp;
+    bp->b_doto = wp->w_doto;
+    curbp = obp;
+    curwp = owp;
+    bp->b_flag &= ~BFCHG;
+    unlink(result);
+    return s;
+}
+
+
+/*
+ * COMMAND: c-next-error
+ * 91.09.14  Modified by Y.Koyanagi
+ * 00.03.30  Modified by amura for jump.c
+ * goto next error using *compilation* buffer.
+ */
+/*ARGSUSED*/
+nexterror(f, n)
+{
+    register BUFFER	*bp, *obp;
+    register WINDOW	*wp, *owp;
+    register int	s;
+
+    if (strcmp(curbp->b_bname,"*compilation*") == 0) {
+	nextwind(FFRAND,1);
+    }
+    if ((bp = bfind("*compilation*", TRUE)) == NULL) return FALSE;
+    if ((wp = popbuf(bp)) == NULL) return FALSE;
+    obp = curbp; owp = curwp;
+    curbp = bp; curwp = wp;
+    if ((s=jumptoerror(0, 1)) != TRUE) {
+	curbp = obp;
+	curwp = owp;
+	return s;
+    }
+    return TRUE;
+}
+
+#endif	/* JUMPERR */
---
+++ kbd.c
@@ -0,0 +1,619 @@
+/*
+ *		Terminal independent keyboard handling.
+ */
+/* 90.01.29	Modified for Ng 1.0 by S.Yoshida */
+
+/* $Id: kbd.c,v 1.1 2000/06/01 05:29:03 amura Exp $ */
+
+/* $Log: kbd.c,v $
+/* Revision 1.1  2000/06/01 05:29:03  amura
+/* Initial revision
+/*
+*/
+
+#include	"config.h"	/* 90.12.20  by S.Yoshida */
+#include	"def.h"
+#include	"kbd.h"
+#ifdef	UNDO
+#include	"undo.h"
+#endif
+
+#define EXTERN
+#include	"key.h"
+
+#ifndef NO_MACRO
+#include "macro.h"
+#endif
+
+#ifdef	DO_METAKEY
+#ifndef METABIT
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+#define METABIT 0x100
+#else	/* NOT KANJI */
+#define METABIT 0x80
+#endif	/* KANJI */
+#endif	/* METABIT */
+
+#ifdef CANNA
+#include    <canna/jrkanji.h>
+extern jrKanjiStatus ks;
+int henkan( );
+#endif
+
+int use_metakey = TRUE;
+#ifdef _WIN32
+extern int allow_mouse_event; /* define on "winutil.c" */
+#endif
+
+/*
+ * Toggle the value of use_metakey
+ */
+do_meta(f, n)
+{
+	if(f & FFARG)	use_metakey = n > 0;
+	else		use_metakey = !use_metakey;
+	ewprintf("Meta keys %sabled", use_metakey ? "en" : "dis");
+#ifdef	PC9801
+	if (use_metakey)
+		setezkey();
+	else
+		resetezkey();
+#endif
+	return TRUE;
+}
+#endif
+
+#ifdef	BSMAP
+static int bs_map = BSMAP;
+/*
+ * Toggle backspace mapping
+ */
+bsmap(f, n)
+{
+	if(f & FFARG)	bs_map = n > 0;
+	else		bs_map = ! bs_map;
+	ewprintf("Backspace mapping %sabled", bs_map ? "en" : "dis");
+	return TRUE;
+}
+#endif
+
+#ifndef NO_DPROMPT
+#define PROMPTL 80
+  char	prompt[PROMPTL], *promptp;
+#endif
+
+static	int	pushed = FALSE;
+static	int	pushedc;
+
+VOID	ungetkey(c)
+int	c;
+{
+#ifdef	DO_METAKEY
+	if(use_metakey && pushed && c==CCHR('[')) pushedc |= METABIT;
+	else
+#endif
+		pushedc = c;
+#ifdef 	BSMAP
+	if(bs_map)
+		if(pushedc==CCHR('H')) pushedc=CCHR('?');
+		else if(pushedc==CCHR('?')) pushedc=CCHR('H');
+#endif	
+	pushed = TRUE;
+}
+
+int getkey(flag)
+int	flag;
+{
+	int	c;
+	char	*keyname();
+
+#ifndef NO_DPROMPT
+	if(flag && !pushed) {
+		if(prompt[0]!='\0' && ttwait()) {
+			ewprintf("%s", prompt);	/* avoid problems with % */
+			update();		/* put the cursor back	 */
+			epresf = KPROMPT;
+		}
+		if(promptp > prompt) *(promptp-1) = ' ';
+	}
+#endif
+	if(pushed) {
+		c = pushedc;
+		pushed = FALSE;
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+	} else	c = kgetkey();
+#else	/* NOT KANJI */
+	} else	c = getkbd();
+#endif	/* KANJI */
+#ifdef 	BSMAP
+	if(bs_map)
+		if(c==CCHR('H')) c=CCHR('?');
+		else if(c==CCHR('?')) c=CCHR('H');
+#endif	
+#ifdef	DO_METAKEY
+	if(use_metakey && (c&METABIT)) {
+		pushedc = c & ~METABIT;
+		pushed = TRUE;
+		c = CCHR('[');
+	}
+#endif
+#ifndef NO_DPROMPT
+	if(flag && promptp < &prompt[PROMPTL - 5]) {
+	    promptp = keyname(promptp, c);
+	    *promptp++ = '-';
+	    *promptp = '\0';
+	}
+#endif
+	return c;
+}
+
+/*
+ * doscan scans a keymap for a keyboard character and returns a pointer
+ * to the function associated with that character.  Sets ele to the
+ * keymap element the keyboard was found in as a side effect.
+ */
+
+MAP_ELEMENT *ele;
+
+PF	doscan(map, c)
+register KEYMAP *map;
+register int	c;
+{
+    register MAP_ELEMENT *elec = &map->map_element[0];	/* local register copy for faster access */
+    register MAP_ELEMENT *last = &map->map_element[map->map_num];
+    PF funct;
+
+    while(elec < last && c > elec->k_num) elec++;
+    ele = elec;			/* used by prefix and binding code	*/
+    if(elec >= last || c < elec->k_base)
+	funct = map->map_default;
+    else
+	funct = elec->k_funcp[c - elec->k_base];
+    return funct;
+}
+
+doin()
+{
+	KEYMAP	*curmap;
+	PF	funct;
+	int d;
+#ifdef	UNDO
+	int s;
+	BUFFER *bp;
+#endif
+
+#ifndef NO_DPROMPT
+    *(promptp = prompt) = '\0';
+#endif
+#ifdef FEPCTRL	/* 90.11.26  by K.Takano */
+    fepmode_on();
+#endif
+#ifdef _WIN32
+    allow_mouse_event = TRUE;
+#endif
+    curmap = curbp->b_modes[curbp->b_nmodes]->p_map;
+    key.k_count = 0;
+    d=getkey(TRUE);
+#ifdef CANNA
+    if( (curbp->b_flag & BFCANNA) &&
+        (ks.length != 0 || !(d==' '||ISCTRL(d)||ISKANJI(d))) )
+	return henkan(d);
+    else
+#endif
+    while((funct=doscan(curmap,(key.k_chars[key.k_count++]=d)))
+		== prefix) {
+#ifdef FEPCTRL	/* 90.11.26  by K.Takano */
+	fepmode_off();
+#endif
+#ifdef _WIN32
+	allow_mouse_event = FALSE;
+#endif
+	curmap = ele->k_prefmap;
+	d = getkey(TRUE);
+    }
+#ifndef NO_MACRO
+    if(macrodef && macrocount < MAXMACRO)
+	macro[macrocount++].m_funct = funct;
+#endif
+    if (epresf == TRUE)
+      eerase();
+#ifdef	UNDO
+    bp = curbp;
+    ublock_open(bp);
+    s = (*funct)(0, 1);
+    ublock_close(bp);
+    return s;
+#else
+    return (*funct)(0, 1);
+#endif
+}
+
+rescan(f, n)
+int f, n;
+{
+    int c;
+    register KEYMAP *curmap;
+    int i;
+    PF	fp = (PF)NULL;
+    int mode = curbp->b_nmodes;
+
+    for(;;) {
+	if(ISUPPER(key.k_chars[key.k_count-1])) {
+	    c = TOLOWER(key.k_chars[key.k_count-1]);
+	    curmap = curbp->b_modes[mode]->p_map;
+	    for(i=0; i < key.k_count-1; i++) {
+		if((fp=doscan(curmap,(key.k_chars[i]))) != prefix) break;
+		curmap = ele->k_prefmap;
+	    }
+	    if(fp==prefix) {
+		if((fp = doscan(curmap, c)) == prefix) {
+#ifdef FEPCTRL	/* 90.11.26  by K.Takano */
+		    fepmode_off();
+#endif
+		    while((fp=doscan(curmap,key.k_chars[key.k_count++] =
+			    getkey(TRUE))) == prefix)
+			curmap = ele->k_prefmap;
+		}
+		if(fp!=rescan) {
+#ifndef NO_MACRO
+		    if(macrodef && macrocount <= MAXMACRO)
+			macro[macrocount-1].m_funct = fp;
+#endif
+		    return (*fp)(f, n);
+		}
+	    }
+	}
+	/* try previous mode */
+	if(--mode < 0) return ABORT;
+	curmap = curbp->b_modes[mode]->p_map;
+	for(i=0; i < key.k_count; i++) {
+	    if((fp=doscan(curmap,(key.k_chars[i]))) != prefix) break;
+	    curmap = ele->k_prefmap;
+	}
+	if(fp==prefix) {
+#ifdef FEPCTRL	/* 90.11.26  by K.Takano */
+	    fepmode_off();
+#endif
+	    while((fp=doscan(curmap,key.k_chars[i++]=getkey(TRUE)))
+		    == prefix)
+		curmap = ele->k_prefmap;
+	    key.k_count = i;
+	}
+	if(fp!=rescan && i>=key.k_count-1) {
+#ifndef NO_MACRO
+	    if(macrodef && macrocount <= MAXMACRO)
+		macro[macrocount-1].m_funct = fp;
+#endif
+	    return (*fp)(f, n);
+	}
+    }
+}
+
+int universal_argument pro((int, int));
+
+universal_argument(f, n)
+int f, n;
+{
+    int c, nn=4;
+    KEYMAP *curmap;
+    PF	funct;
+
+#ifdef FEPCTRL	/* 90.11.26  by K.Takano */
+    fepmode_off();
+#endif
+    if(f&FFUNIV) nn *= n;
+    for(;;) {
+	key.k_chars[0] = c = getkey(TRUE);
+	key.k_count = 1;
+	if(c == '-') return negative_argument(f, nn);
+	if(c >= '0' && c <= '9') return digit_argument(f, nn);
+	curmap = curbp->b_modes[curbp->b_nmodes]->p_map;
+	while((funct=doscan(curmap,c)) == prefix) {
+	    curmap = ele->k_prefmap;
+	    key.k_chars[key.k_count++] = c = getkey(TRUE);
+	}
+	if(funct != universal_argument) {
+#ifndef NO_MACRO
+	    if(macrodef && macrocount < MAXMACRO-1) {
+		if(f&FFARG) macrocount--;
+		macro[macrocount++].m_count = nn;
+		macro[macrocount++].m_funct = funct;
+	    }
+#endif
+	    return (*funct)(FFUNIV, nn);
+	}
+	nn <<= 2;
+    }
+}
+
+/*ARGSUSED*/
+digit_argument(f, n)
+int f, n;
+{
+    int nn, c;
+    KEYMAP *curmap;
+    PF	funct;
+
+#ifdef FEPCTRL	/* 90.11.26  by K.Takano */
+    fepmode_off();
+#endif
+    nn = key.k_chars[key.k_count-1] - '0';
+    for(;;) {
+	c = getkey(TRUE);
+	if(c < '0' || c > '9') break;
+	nn *= 10;
+	nn += c - '0';
+    }
+    key.k_chars[0] = c;
+    key.k_count = 1;
+    curmap = curbp->b_modes[curbp->b_nmodes]->p_map;
+    while((funct=doscan(curmap,c)) == prefix) {
+	curmap = ele->k_prefmap;
+	key.k_chars[key.k_count++] = c = getkey(TRUE);
+    }
+#ifndef NO_MACRO
+    if(macrodef && macrocount < MAXMACRO-1) {
+	if(f&FFARG) macrocount--;
+	else macro[macrocount-1].m_funct = universal_argument;
+	macro[macrocount++].m_count = nn;
+	macro[macrocount++].m_funct = funct;
+    }
+#endif
+    return (*funct)(FFOTHARG, nn);
+}
+
+negative_argument(f, n)
+int f, n;
+{
+    int nn = 0, c;
+    KEYMAP *curmap;
+    PF	funct;
+
+#ifdef FEPCTRL	/* 90.11.26  by K.Takano */
+    fepmode_off();
+#endif
+    for(;;) {
+	c = getkey(TRUE);
+	if(c < '0' || c > '9') break;
+	nn *= 10;
+	nn += c - '0';
+    }
+    if(nn) nn = -nn;
+    else nn = -n;
+    key.k_chars[0] = c;
+    key.k_count = 1;
+    curmap = curbp->b_modes[curbp->b_nmodes]->p_map;
+    while((funct=doscan(curmap,c)) == prefix) {
+	curmap = ele->k_prefmap;
+	key.k_chars[key.k_count++] = c = getkey(TRUE);
+    }
+#ifndef NO_MACRO
+    if(macrodef && macrocount < MAXMACRO-1) {
+	if(f&FFARG) macrocount--;
+	else macro[macrocount-1].m_funct = universal_argument;
+	macro[macrocount++].m_count = nn;
+	macro[macrocount++].m_funct = funct;
+    }
+#endif
+    return (*funct)(FFNEGARG, nn);
+}
+
+/*
+ * Insert a character.	While defining a macro, create a "LINE" containing
+ * all inserted characters.
+ */
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+int	no_k2nd = FALSE;		/* We have no KANJI 2nd byte	*/
+int	inkfill = FALSE;		/* Now we are in a fillword().	*/
+#endif	/* KANJI */
+
+selfinsert(f, n)
+int f, n;
+{
+    register int c;
+    int count;
+    VOID lchange();
+#ifndef NO_MACRO
+    LINE *lp;
+    int insert pro((int, int));
+#endif
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+    int	lkanji2nd;			/* For over write mode.	*/
+#endif	/* KANJI */
+
+    if (n < 0)	return FALSE;
+    if (n == 0) return TRUE;
+    c = key.k_chars[key.k_count-1];
+#ifdef	READONLY	/* 91.01.05  by S.Yoshida */
+    if (curbp->b_flag & BFRONLY) {	/* If this buffer is read-only, */
+	warnreadonly();			/* do only displaying warning.	*/
+	return TRUE;
+    }
+#endif	/* READONLY */
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+    /* In a autofill mode, normally fill trigger is only ' ' char. But	*/
+    /* KANJI version require any KANJI char is also fill trigger.	*/
+    /* This must be done at keymap.c to add KANJI fill trigger list in	*/
+    /* a fillmap. But there are too many KANJI chars, so we use this	*/
+    /* easy way.							*/
+    if(curbp->b_flag & BFAUTOFILL && 		/* Autofill mode and	*/
+       !inkfill && no_k2nd) {			/* KANJI 2nd byte.	*/
+	    int	s;
+	    inkfill = TRUE;
+	    s = fillword(f, n);		/* fill word with KANJI char.	*/
+	    inkfill = FALSE;
+	    return (s);
+    }					/* End of autofill mode add routine. */
+    if (no_k2nd) {			/* If there is only KANJI 1st byte, */
+	    no_k2nd = FALSE;		/* we believe 'c' is KANJI 2nd byte.*/
+    } else if (ISKANJI(c)) {
+	    if ((n % 2) == 0) {		/* This is easy bug fix. */
+		    n |= 0x01;
+	    }
+	    no_k2nd = TRUE;		/* When there is no KANJI 2nd	*/
+					/* byte, we don't do update().	*/
+    }
+#endif	/* KANJI */
+#ifndef NO_MACRO
+    if(macrodef && macrocount < MAXMACRO) {
+	if(f & FFARG) macrocount -= 2;
+	if(lastflag & CFINS) {	/* last command was insert -- tack on end */
+	    macrocount--;
+	    if(maclcur->l_size < maclcur->l_used + n) {
+		if((lp = lallocx(maclcur->l_used + n)) == NULL)
+		    return FALSE;
+		lp->l_fp = maclcur->l_fp;
+		lp->l_bp = maclcur->l_bp;
+		lp->l_fp->l_bp = lp->l_bp->l_fp = lp;
+		bcopy(maclcur->l_text, lp->l_text, maclcur->l_used);
+		for(count = maclcur->l_used; count < lp->l_used; count++)
+		    lp->l_text[count] = c;
+		free((char *)maclcur);
+		maclcur = lp;
+	    } else {
+		maclcur->l_used += n;
+		for(count = maclcur->l_used-n; count < maclcur->l_used; count++)
+		    maclcur->l_text[count] = c;
+	    }
+	} else {
+	    macro[macrocount-1].m_funct = insert;
+	    if((lp = lallocx(n)) == NULL) return FALSE;
+	    lp->l_bp = maclcur;
+	    lp->l_fp = maclcur->l_fp;
+	    maclcur->l_fp = lp;
+	    maclcur = lp;
+	    for(count = 0; count < n; count++)
+		lp->l_text[count] = c;
+	}
+	thisflag |= CFINS;
+    }
+#endif
+    if(c == '\n') {
+	do {
+	    count = lnewline();
+	} while (--n && count==TRUE);
+	return count;
+    }
+#ifdef	UNDO
+    if (isundo()) {
+	thisflag |= CFINS2;
+	if(curbp->b_flag & BFOVERWRITE) {	/* Overwrite mode	*/
+	    UNDO_DATA *undo;
+	    if (lastflag & CFINS2) {
+		curbp->b_utop--;
+		if (curbp->b_utop < 0)
+		    curbp->b_utop = UNDOSIZE;
+		undoptr = &curbp->b_ustack[curbp->b_utop];
+		if (undo_type(*undoptr) != UDOVER)
+		    panic("kbd: Run overwrite error");
+		undo = *undoptr;
+	    } else {
+		undo_setup(undo);
+		if (!isundo())
+		    goto noundo;
+		undo->u_dotlno = get_lineno(curbp,curwp->w_dotp);
+		undo->u_doto = curwp->w_doto;
+		undo->u_type = UDOVER;
+		undo->u_used = 0;
+		undo->u_code[0] = '\0';
+	    }
+	    undo_bgrow(undo, n);
+
+	    lchange(WFEDIT);
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+	    if (undo->u_code[0]) {
+		lputc(curwp->w_dotp, curwp->w_doto, undo->u_code[0]);
+		lkanji2nd = TRUE;
+	    } else
+		lkanji2nd = FALSE;
+#endif	/* KANJI */
+	    count = undo->u_used;
+	    while(curwp->w_doto < llength(curwp->w_dotp) && n--) {
+		undo->u_buffer[count] =
+		    lgetc(curwp->w_dotp, curwp->w_doto);
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+		if (lkanji2nd) {
+		    lkanji2nd = FALSE;
+		} else if (ISKANJI(undo->u_buffer[count])){
+		    lkanji2nd = TRUE;
+		}
+#endif	/* KANJI */
+		lputc(curwp->w_dotp, curwp->w_doto++, c);
+		count++;
+	    }
+	    undo->u_used = count;
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+	    if (lkanji2nd && curwp->w_doto < llength(curwp->w_dotp)) {
+		undo->u_code[0] = lgetc(curwp->w_dotp, curwp->w_doto);
+		lputc(curwp->w_dotp, curwp->w_doto, ' ');
+	    } else
+		undo->u_code[0] = '\0';
+#endif	/* KANJI */
+	    undo_finish(&(undo->u_next));
+
+	    if ((lastflag&CFINS2) == 0) {
+		if (undoptr!=NULL && *undoptr!=NULL)
+		    (*undoptr)->u_type = UDNONE;
+	    }
+	    if (n<=0) return TRUE;
+	} else if (lastflag & CFINS2) {
+	    curbp->b_utop--;
+	    if (curbp->b_utop < 0)
+		curbp->b_utop = UNDOSIZE;
+	    undoptr = &curbp->b_ustack[curbp->b_utop];
+	    if (undo_type(*undoptr) != UDINS)
+		panic("kbd: Run insert error");
+	}
+	/* if this is NOT, somecase linsert() panic */
+	  else if (*undoptr != NULL)
+	    (*undoptr)->u_type = UDNONE;
+    } else
+  noundo:
+#endif	/* UNDO */
+    if(curbp->b_flag & BFOVERWRITE) {		/* Overwrite mode	*/
+	lchange(WFEDIT);
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+	lkanji2nd = FALSE;
+#endif	/* KANJI */
+	while(curwp->w_doto < llength(curwp->w_dotp) && n--) {
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+	    if (lkanji2nd) {
+		lkanji2nd = FALSE;
+	    } else if (ISKANJI(lgetc(curwp->w_dotp, curwp->w_doto))) {
+		lkanji2nd = TRUE;
+	    }
+#endif	/* KANJI */
+	    lputc(curwp->w_dotp, curwp->w_doto++, c);
+	}
+#ifdef	KANJI	/* 90.01.29  by S.Yoshida */
+	if (lkanji2nd && curwp->w_doto < llength(curwp->w_dotp)) {
+	    lputc(curwp->w_dotp, curwp->w_doto, ' ');
+	}
+#endif	/* KANJI */
+	if(n<=0) return TRUE;
+    }
+    return linsert(n, c);
+}
+
+/*
+ * this could be implemented as a keymap with everthing defined
+ * as self-insert.
+ */
+quote(f, n)
+{
+    register int c;
+
+    key.k_count = 1;
+    if((key.k_chars[0] = getkey(TRUE)) >= '0' && key.k_chars[0] <= '7') {
+	key.k_chars[0] -= '0';
+	if((c = getkey(TRUE)) >= '0' && c <= '7') {
+	    key.k_chars[0] <<= 3;
+	    key.k_chars[0] += c - '0';
+	    if((c = getkey(TRUE)) >= '0' && c <= '7') {
+		key.k_chars[0] <<= 3;
+		key.k_chars[0] += c - '0';
+	    } else ungetkey(c);
+	} else ungetkey(c);
+    }
+    return selfinsert(f, n);
+}
