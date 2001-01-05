92e90ab59c49dfb598da81d8a25a26ca6e71a58c
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2001-01-06 02:28:33 +0900

    edit to 1.4.3 release

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-/* $Id: version.c,v 1.8 2000/11/16 14:27:00 amura Exp $ */
+/* $Id: version.c,v 1.8.2.1 2001/01/05 17:28:33 amura Exp $ */
 /*
  * This file contains the string that get written
  * out by the emacs-version command.
@@ -6,6 +6,9 @@
 
 /*
  * $Log: version.c,v $
+ * Revision 1.8.2.1  2001/01/05 17:28:33  amura
+ * edit to 1.4.3 release
+ *
  * Revision 1.8  2000/11/16 14:27:00  amura
  * edit to 1.4.2 release
  *
@@ -45,33 +48,33 @@
 # ifdef	MSDOS			/* 90.02.11  by S.Yoshida */
 #  ifdef TCCONIO
 #   ifdef IBMPC			/* 90.03.10  by S.Yoshida */
-char version[] = "Ng 1.4.2 for IBM PC/TCCONIO [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 for IBM PC/TCCONIO [Nihongo Mg 2a] ";
 #   else /* NOT IBMPC */
 #    ifdef PC9801		/* 90.03.10  by S.Yoshida */
-char version[] = "Ng 1.4.2 for PC-9801/TCCONIO [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 for PC-9801/TCCONIO [Nihongo Mg 2a] ";
 #    else /* NOT PC9801 */
-char version[] = "Ng 1.4.2 for MS-DOS/TCCONIO [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 for MS-DOS/TCCONIO [Nihongo Mg 2a] ";
 #    endif /* PC9801 */
 #   endif /* IBMPC */
 #  else /* NOT TCCONIO */
 #   ifdef IBMPC			/* 90.03.10  by S.Yoshida */
-char version[] = "Ng 1.4.2 for IBM PC [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 for IBM PC [Nihongo Mg 2a] ";
 #   else /* NOT IBMPC */
 #    ifdef PC9801		/* 90.03.10  by S.Yoshida */
-char version[] = "Ng 1.4.2 for PC-9801 [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 for PC-9801 [Nihongo Mg 2a] ";
 #    else /* NOT PC9801 */
-char version[] = "Ng 1.4.2 for MS-DOS [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 for MS-DOS [Nihongo Mg 2a] ";
 #    endif /* PC9801 */
 #   endif /* IBMPC */
 #  endif /* TCCONIO */
 # else /* NOT MSDOS */
 #  ifdef AMIGA			/* by H.Ohkubo / H.Konishi */
-char version[] = "Ng 1.4.2 / KANgee ver 4.3 [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 / KANgee ver 4.3 [Nihongo Mg 2a] ";
 #  else /* NOT AMIGA */
 #   ifdef WIN32
-char version[] = "Ng 1.4.2 / Win32 v0.5.24 [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 / Win32 v0.5.24 [Nihongo Mg 2a] ";
 #   else
-char version[] = "Ng 1.4.2 [Nihongo Mg 2a] ";
+char version[] = "Ng 1.4.3 [Nihongo Mg 2a] ";
 #   endif /* WIN32 */
 #  endif /* AMIGA */
 # endif /* MSDOS */
