f855813306610d3f1ffaca4be618471b38feb428
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2003-03-03 03:39:50 +0900

    change curwp to curbp

--- sys/amiga/ttymouse.c
+++ sys/amiga/ttymouse.c
@@ -1,4 +1,4 @@
-/* $Id: ttymouse.c,v 1.2 2001/11/23 11:56:46 amura Exp $ */
+/* $Id: ttymouse.c,v 1.1.1.1.4.1 2003/03/02 18:39:50 amura Exp $ */
 /*
  * Name:	MG 2a
  *		Commodore Amiga mouse handling 
@@ -7,8 +7,8 @@
  */
 /*
  * $Log: ttymouse.c,v $
- * Revision 1.2  2001/11/23 11:56:46  amura
- * Rewrite all sources
+ * Revision 1.1.1.1.4.1  2003/03/02 18:39:50  amura
+ * change curwp to curbp
  *
  * Revision 1.1.1.1  2000/06/27 01:48:01  amura
  * import to CVS
@@ -19,33 +19,33 @@
 #include <intuition/intuition.h>
 #undef	TRUE
 #undef	FALSE
-#include "config.h"	/* Dec.19,1992 Add by H.Ohkubo */
+#include	"config.h"	/* Dec.19,1992 Add by H.Ohkubo */
 #include "def.h"
 #ifndef	NO_MACRO
 #include "macro.h"
 #endif
 
-extern	int	forwline _PRO((int,int));
-extern	int	forwchar _PRO((int,int));
-extern	int	setmark _PRO((int,int));
-extern	int	isetmark _PRO((void));
+extern	int	forwline pro((int,int));
+extern	int	forwchar pro((int,int));
+extern	int	setmark pro((int,int));
+extern	int	isetmark pro((void));
 
 /* stuff for go-to-window-and-do-it functions */
-extern	int	reposition _PRO((int,int));
-extern	int	delfword _PRO((int,int));
-extern	int	killline _PRO((int,int));
-extern	int	forwdel _PRO((int,int));
-extern	int	justone _PRO((int,int));
-extern	int	killregion _PRO((int,int));
-extern	int	yank _PRO((int,int));
-extern	int	forwpage _PRO((int,int));
-extern	int	backpage _PRO((int,int));
-extern	int	splitwind _PRO((int,int));
-extern	int	delwind _PRO((int,int));
-extern	int	gotobob _PRO((int,int));
-extern	int	gotoeob _PRO((int,int));
-extern	int	enlargewind _PRO((int,int));
-extern	int	shrinkwind _PRO((int,int));
+extern	int	reposition pro((int,int));
+extern	int	delfword pro((int,int));
+extern	int	killline pro((int,int));
+extern	int	forwdel pro((int,int));
+extern	int	justone pro((int,int));
+extern	int	killregion pro((int,int));
+extern	int	yank pro((int,int));
+extern	int	forwpage pro((int,int));
+extern	int	backpage pro((int,int));
+extern	int	splitwind pro((int,int));
+extern	int	delwind pro((int,int));
+extern	int	gotobob pro((int,int));
+extern	int	gotoeob pro((int,int));
+extern	int	enlargewind pro((int,int));
+extern	int	shrinkwind pro((int,int));
 
 /*
  * Handle the mouse click that's been passed by ttgetc() and position
@@ -55,280 +55,246 @@ extern	int	shrinkwind _PRO((int,int));
  * but it does most of what we want.
  */
 
-static USHORT oldrow = HUGE, oldcol = HUGE;	/* last mouse click	*/
-static USHORT newrow, newcol;			/* next mouse click	*/
-static int dottomouse _PRO((void));
+static USHORT	oldrow = HUGE, oldcol = HUGE;	/* last mouse click	*/
+static USHORT	newrow, newcol;			/* next mouse click	*/
+static dottomouse();
 
-int
 amigamouse(f, n)
-int f, n;
 {
-    if (!dottomouse())			/* sets newrow, newcol	*/
-	return (FALSE);
-    if ((newrow == oldrow) && (newcol == oldcol))
-	setmark(FFRAND, 1);		/* double-click		*/
-    oldrow = newrow;		    	/* save state		*/
-    oldcol = newcol;
-    return (TRUE);
+	if (!dottomouse())			/* sets newrow, newcol	*/
+		return (FALSE);
+	if ((newrow == oldrow) && (newcol == oldcol))
+		setmark(FFRAND, 1);		/* double-click		*/
+	oldrow = newrow;		    	/* save state		*/
+	oldcol = newcol;
+	return (TRUE);
 }
 
 /*
  * Recenter on selected line
  */
-int
 mreposition(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (reposition(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (reposition(f, n));
 }
 
 /*
  * Delete word after selected char
  */
-int
 mdelfword(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (delfword(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (delfword(f, n));
 }
 
 /*
  * Move to selection, kill line
  */
-int
 mkillline(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (killline(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (killline(f, n));
 }
 
 /*
  * Move to selection, kill word
  */
-int
 mforwdel(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (forwdel(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (forwdel(f, n));
 }
 
 /*
  * Move to selection, kill line
  */
-int
 mdelwhite(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (justone(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (justone(f, n));
 }
 
 /*
  * Set mark, move to selection, kill region.
  */
-int
 mkillregion(f, n)
-int f, n;
 {
-    register struct LINE *p_old;
-    register short o_old;
-    
-    p_old = curbp->b_markp;		/* Save old mark */
-    o_old = curbp->b_marko;
-    isetmark();				/* and set current one */
-    if (!dottomouse()) {
-	curbp->b_markp = p_old;		/* Oops - put mark back */
-	curbp->b_marko = o_old;
-	return (FALSE);
-    }
-    return (killregion(f, n));
+	register struct LINE *p_old;
+	register short	o_old;
+
+	p_old = curbp->b_markp;		/* Save old mark */
+	o_old = curbp->b_marko;
+	isetmark();				/* and set current one */
+	if (!dottomouse()) {
+		curbp->b_markp = p_old;	/* Oops - put mark back */
+		curbp->b_marko = o_old;
+		return (FALSE);
+	}
+	return (killregion(f, n));
 }
 
 /*
  * Move to selection, yank kill buffer
  */
-int
 myank(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (yank(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (yank(f, n));
 }
 
 /*
  * Select window pointed to by mouse, then scroll down
  */
-int
+
 mforwpage(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (forwpage(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (forwpage(f, n));
 }
 
 /*
  * Select buffer, scroll page down
  */
-int
 mbackpage(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (backpage(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (backpage(f, n));
 }
 
 /*
  * Select the window, split it.
  */
-int
 msplitwind(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (splitwind(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (splitwind(f, n));
 }
 
 /*
  * Select the buffer, delete it.
  */
-int
 mdelwind(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (delwind(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (delwind(f, n));
 }
 
 /*
  * Select window, goto beginning of buffer
  */
-int
 mgotobob(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (gotobob(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (gotobob(f, n));
 }
 
 /*
  * Select window, go to end of buffer
  */
-int
 mgotoeob(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (gotoeob(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (gotoeob(f, n));
 }
 
 /*
  * Select window, enlarge it.
  */
-int
 menlargewind(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (enlargewind(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (enlargewind(f, n));
 }
 	
 /*
  * Select window, shrink it.
  */
-int
 mshrinkwind(f, n)
-int f, n;
 {
-    if (!dottomouse())
-	return (FALSE);
-    return (shrinkwind(f, n));
+	if (!dottomouse())
+		return (FALSE);
+	return (shrinkwind(f, n));
 }
 
 /*
  * Utility routine to move dot to where the user clicked.  If in
  * mode line, chooses that buffer as the one to work on.
  */
-static int
-dottomouse()
+
+static dottomouse()
 {
-    register WINDOW *wp;
-    register int dot;
-    register int col;
-    register int c;
-    int getkey _PRO((int));
+	register WINDOW *wp;
+	register int	dot;
+	register int	col;
+	register int	c;
+	int		getkey pro((int));
 
 #ifndef	NO_MACRO
-    if (inmacro)
-	return FALSE;	/* can't record mouse clicks */
+	if (inmacro)
+		return FALSE;	/* can't record mouse clicks */
 #endif
 
-    /* read the next 2 characters to get the col, row info, using
-     * getkey() to record them (or re-read them if in a macro).
-     */
-    newcol = getkey(FALSE) - M_X_ZERO;
-    newrow = getkey(FALSE) - M_Y_ZERO;
+	/* read the next 2 characters to get the col, row info, using
+	 * getkey() to record them (or re-read them if in a macro).
+	 */
+	newcol = getkey(FALSE) - M_X_ZERO;
+	newrow = getkey(FALSE) - M_Y_ZERO;
 
 #ifndef	NO_MACRO
-    if (macrodef) {		/* menu picks can't be practically recorded */
-	ewprintf("Can't record mouse clicks");
-	return (FALSE);
-    }
-#endif
-
-    /* find out which window was clicked in			*/
-    for (wp = wheadp; wp != NULL; wp = wp->w_wndp) {
-	if ((newrow >= wp->w_toprow) && 
-	    (newrow <= (wp->w_toprow + wp->w_ntrows)))
-	    break;
-    }
-
-    if (wp == NULL)				/* echo line		*/
-	return (ABORT);
-    else if (newrow == wp->w_toprow + wp->w_ntrows) {/* mode line */
-	curwp = wp;			/* just change buffer	 */
-	curbp = wp->w_bufp;
-    }
-    else {
-	/* move to selected window, move dot to top left	*/
-	curwp = wp;
-	curbp = wp->w_bufp;
-	curwp->w_dotp = wp->w_linep;
-	curwp->w_doto = 0;
-	
-	/* go forward the correct # of lines 		*/
-	forwline(FFRAND, newrow - curwp->w_toprow);
-	
-	/* go forward the correct # of characters	*/
-	/* need to count them out because of tabs	*/
-	col = dot = 0;
-	while ((col < newcol) && (dot < llength(curwp->w_dotp))) {
-	    c = lgetc(curwp->w_dotp, dot++);
-	    if (c == CCHR('I'))
-		col |= 0x07;
-	    else if (ISCTRL(c) != FALSE)
-		++col;
-	    ++col;
+	if (macrodef) {		/* menu picks can't be practically recorded */
+		ewprintf("Can't record mouse clicks");
+		return (FALSE);
 	}
-	if (col > newcol)	/* back up to tab/ctrl char */
-	    dot--;
-	forwchar(FFRAND, dot);
-    }
-    return (TRUE);
+#endif
+
+	/* find out which window was clicked in				*/
+	for (wp = wheadp; wp != NULL; wp = wp->w_wndp)
+		if ((newrow >= wp->w_toprow) && 
+			(newrow <= (wp->w_toprow + wp->w_ntrows)))
+			break;
+
+	if (wp == NULL)				/* echo line		*/
+		return (ABORT);
+	else if (newrow == wp->w_toprow + wp->w_ntrows) {/* mode line */
+		curwp = wp;			/* just change buffer	 */
+		curbp = wp->w_bufp;
+	} else {
+		/* move to selected window, move dot to top left	*/
+		curwp = wp;
+		curbp = wp->w_bufp;
+		curwp->w_dotp = wp->w_linep;
+		curwp->w_doto = 0;
+
+		/* go forward the correct # of lines 		*/
+		forwline(FFRAND, newrow - curwp->w_toprow);
+	
+		/* go forward the correct # of characters	*/
+		/* need to count them out because of tabs	*/
+		col = dot = 0;
+		while ((col < newcol) && (dot < llength(curwp->w_dotp))) {
+			c = lgetc(curwp->w_dotp, dot++);
+			if (c == CCHR('I'))
+				col |= 0x07;
+			else if (ISCTRL(c) != FALSE)
+				++col;
+			++col;
+		}
+		if (col > newcol) dot--;	/* back up to tab/ctrl char */
+		forwchar(FFRAND, dot);
+	}
+	return (TRUE);
 }
